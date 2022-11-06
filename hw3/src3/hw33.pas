9816243d6e1e17e3968945365b62c337a617578d
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-11-06 15:45:10 +0300

    rk2

--- hw3/src3/hw33.pas
+++ hw3/src3/hw33.pas
@@ -1,21 +1,23 @@
 program hw33;
 
-type tbest5 = array[1..5] of real;
-
 type Horse = object
   private
     name: String;
     best: array[1..5] of real;
   public
-    procedure Init(nname: String; nbest: tbest5);
+    constructor Init(nname: string; best1: real; best2: real; best3: real; best4: real; best5: real);
     procedure Print;
     function AverageTime: real;
 end;
 
-procedure Horse.Init(nname: String; nbest: tbest5);
+constructor Horse.Init(nname: string; best1: real; best2: real; best3: real; best4: real; best5: real);
 begin
   name := nname;
-  best := nbest;
+  best[1] := best1;
+  best[2] := best2;
+  best[3] := best3;
+  best[4] := best4;
+  best[5] := best5;
 end;
 
 type thorses = array of horse;
@@ -25,7 +27,7 @@ var i: integer;
 begin
   WriteLn('Horse { name: ', name, ' best:');
   for i := 1 to 5 do
-    WriteLn('time: ', best[i]);
+    WriteLn('time: ', best[i]:4:1);
   WriteLn('}');
 end;
 
@@ -33,6 +35,7 @@ function Horse.AverageTime: real;
 var i: integer;
     sum: real;
 begin
+  sum := 0;
   for i := 1 to 5 do
     sum += best[i];
   Result := sum / 5;
@@ -42,12 +45,12 @@ type Stable = object
   private
     horses: thorses;
   public
-    procedure Init(nhorses: thorses);
+    constructor Init(nhorses: thorses);
     procedure Print;
     function AverageTime: real;
 end;
 
-procedure Stable.Init(nhorses: thorses);
+constructor Stable.Init(nhorses: thorses);
 begin
   horses := nhorses;
 end;
@@ -65,11 +68,30 @@ function Stable.AverageTime: real;
 var i: integer;
     sum: real;
 begin
+  sum := 0;
   for i := 0 to high(horses) do
     sum += horses[i].AverageTime;
   Result := sum / (high(horses)+1);
 end;
 
-var
+var my_stable: Stable;
+    my_horses: thorses;
 begin
+  SetLength(my_horses, 5);
+  my_horses[0].Init('1', 1.0, 1.1, 1.2, 1.3, 1.4);
+  my_horses[1].Init('2', 2.0, 2.1, 2.2, 2.3, 2.4);
+  my_horses[2].Init('3', 3.0, 3.1, 3.2, 3.3, 3.4);
+  my_horses[3].Init('4', 4.0, 4.1, 4.2, 4.3, 4.4);
+  my_horses[4].Init('5', 5.0, 5.1, 5.2, 5.3, 5.4);
+
+  my_stable.Init(my_horses);
+
+  WriteLn('Horse 2: ');
+  my_horses[1].Print;
+  WriteLn('Stable: ');
+  my_stable.Print;
+
+  WriteLn('Average time of horse 2: ', my_horses[1].AverageTime:4:1);
+  WriteLn('Average time of all horses: ', my_stable.AverageTime:4:1);
+  ReadLn;
 end.
---
+++ rk2/src/rk2.lpr
@@ -0,0 +1,52 @@
+﻿program rk2;
+type arr = array[1..50, 1..50] of integer;
+
+procedure InputArr(var a: arr; n: integer);
+var i, j: integer;
+begin
+  for i := 1 to n do
+    for j := 1 to n do
+      Read(a[i, j]);
+  ReadLn;
+end;
+
+var a: arr;
+    n, k, i, j,
+    min_left, min_right,
+    left_i, right_i: integer;
+begin
+  Write('Введите N:> ');
+  ReadLn(n);
+  Write('Введите K:> ');
+  ReadLn(k);
+
+  InputArr(a, n);
+
+  min_left := 1000000;
+  min_right := 1000000;
+  for i := 1 to n do
+    for j := 1 to n do
+    begin
+      if i+j <= k then
+      begin
+        if a[i, j] < min_left then
+        begin
+          min_left := a[i, j];
+          left_i := j + (i-1)*n;
+        end;
+      end
+      else
+      begin
+        if a[i, j] < min_right then
+        begin
+          min_right := a[i, j];
+          right_i := j + (i-1)*n;
+        end;
+      end;
+    end;
+
+  WriteLn('До (и на) линии минимум ', min_left, ' с порядковым номером ', left_i);
+  WriteLn('После линии минимум ', min_right, ' с порядковым номером ', right_i);
+  ReadLn;
+end.
+
